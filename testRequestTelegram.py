185916887e03d77f12cc545d46fe7b5d899a3014
raphael.gleye <raphael.gleyeY78@gmail.com>
2022-09-20 18:15:44 +0200

    Version simplifiée python

--- testRequestTelegram.py
+++ testRequestTelegram.py
@@ -1,11 +1,6 @@
 # Test d'envoi de requête contenant un simple message texte sur un bot telegram
 import requests
 
-token="5647128476:AAHgpPJ625ZpgXkIayld5_qF4XiXBvTZVtM"
-url = f"https://api.telegram.org/bot{token}"
-contenu="bonjour"
-
-chat_id = "5288792176" # Mon id perso
-params = {"chat_id": chat_id, "text": contenu}
-r = requests.get(url + "/sendMessage", params=params)
+url = "https://api.telegram.org/bot5647128476:AAHgpPJ625ZpgXkIayld5_qF4XiXBvTZVtM/sendMessage?chat_id=5288792176&text=bonjour"
+r = requests.get(url)
 print(r)
