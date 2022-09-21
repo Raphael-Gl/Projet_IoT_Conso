06fad50644eb39050a51dc22cd6214a7ebf7c191
Raphael <80249378+Raphael-Gl@users.noreply.github.com>
2022-09-22 00:30:40 +0200

    Adaptation au montage et version compacte

--- Capteurs/Capteurs.ino
+++ Capteurs/Capteurs.ino
@@ -2,66 +2,35 @@
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BME680.h"
-#define BME_SCK 14  // Connect "SCL" pin on the BME680 with GPIO 14 on the ESP32
-#define BME_MISO 12 // Connect "SDO" pin on the BME680 with GPIO 12 on the ESP32
-#define BME_MOSI 13 // Connect "SDA" pin on the BME680 with GPIO 13 on the ESP32
-#define BME_CS 15   // Connect "CS"  pin on the BME680 with GPIO 15 on the ESP32
-#define SEALEVELPRESSURE_HPA (1013.25)
-Adafruit_BME680 bme(BME_CS, BME_MOSI, BME_MISO,  BME_SCK);
 
+#define PIN_BRIGHTNESS 36
+int luminosite=0;
 
-
-const int sensorPin = 36;
-int lum = 0;
-// variables:
-int sensorValue = 0;         // the sensor value
-int sensorMin = 4095;        // minimum sensor value
-int sensorMax = 0;           // maximum sensor value
-
-
-
+Adafruit_BME680 bme; // I2C
+//Adafruit_BME680 bme(BME_CS); // hardware SPI
+//Adafruit_BME680 bme(BME_CS, BME_MOSI, BME_MISO,  BME_SCK);
 
 void setup() {
-
+  pinMode(PIN_BRIGHTNESS,INPUT);
   Serial.begin(9600);
   while (!Serial);
   Serial.println(F("BME680 test"));
+
   if (!bme.begin()) {
     Serial.println("Could not find a valid BME680 sensor, check wiring!");
     while (1);
   }
+
   // Set up oversampling and filter initialization
   bme.setTemperatureOversampling(BME680_OS_8X);
   bme.setHumidityOversampling(BME680_OS_2X);
-
-  Serial.begin(115200);
-  delay(1000);
-
-  pinMode(2, OUTPUT);
-  digitalWrite(2, HIGH);
-
-  while (millis() < 10000) {
-    sensorValue = analogRead(sensorPin);
-
-    // record the maximum sensor value
-    if (sensorValue > sensorMax) {
-      sensorMax = sensorValue;
-    }
-
-    // record the minimum sensor value
-    if (sensorValue < sensorMin) {
-      sensorMin = sensorValue;
-    }
-  }
-  
-  digitalWrite(2, LOW);
-
-
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150); // 320*C for 150 ms
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
- if (! bme.performReading()) {
+  if (! bme.performReading()) {
     Serial.println("Failed to perform reading :(");
     return;
   }
@@ -73,20 +42,11 @@ void loop() {
   Serial.print(bme.humidity);
   Serial.println(" %");
 
-  sensorValue = analogRead(sensorPin);
-
-  // in case the sensor value is outside the range seen during calibration
-  sensorValue = constrain(sensorValue, sensorMin, sensorMax);
-
-  // apply the calibration to the sensor reading
-  sensorValue = map(sensorValue, sensorMin, sensorMax, 0, 255);
-
-  // put your main code here, to run repeatedly:
-  //lum = analogRead(sensorPin);
-  Serial.println("Luminosity = ");
-  Serial.print(sensorValue);
+  luminosite=analogRead(PIN_BRIGHTNESS);
+  Serial.print("Luminosite = ");
+  Serial.print(luminosite);
   
-  delay(1000);
 
-
+  Serial.println();
+  delay(2000);
 }
