8c050d1c01ab84ac7e50f0d5525e4b8220a46713
raphael.gleye <raphael.gleyeY78@gmail.com>
2022-09-21 11:06:55 +0200

    Changement de clé, résolution de pb

--- MQTT/publish.ino
+++ MQTT/publish.ino
@@ -2,13 +2,14 @@
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
 
-#define WLAN_SSID "Moimoimoimoimoimoi"
-#define WLAN_PASS "wifideraphael"
+const char *ssid = "Moimoimoimoimoimoi";
+const char *password = "wifideraphael";
+
 #define AIO_SERVER "io.adafruit.com"
 #define AIO_SERVERPORT 1883 // use 8883 for SSL
 
 #define AIO_USERNAME  "Raphael_IoT"
-#define AIO_KEY       "aio_nbBS48H4ISbJ9hTCng9dseSfB6zp"
+#define AIO_KEY       "aio_pOan511OPzui0b9ZYwfRFDmalnAU"
 
 WiFiClient client;
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
@@ -20,16 +21,23 @@ void MQTT_publish(float payload);
 void setup() {
     Serial.begin(9600);
     delay(10);
-   
-    WiFi.begin(WLAN_SSID, WLAN_PASS);
-    while (WiFi.status() != WL_CONNECTED)
-    {
+
+    // We start by connecting to a WiFi network
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
     }
-    Serial.println();
+
+    Serial.println("");
     Serial.println("WiFi connected");
-   
+
 }
 
 
